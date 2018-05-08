61508ecb205d6825fb09a379968fc8f17e808b28
DinAsh12 <dineshbalakrishnan12@gmail.com>
2018-05-08 14:11:32 -0400

    added IIFE to app.js

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+/* Custom JS goes here */
+
+// IIFE - Immediately Invoked Function Expression
+// Anonymous self executing function
+(function(){
+
+    function Start() {
+        console.log("App Started!")
+        console.log("Hello World! I am ROBO.")
+    }
+
+    window.onload = Start;
+})();
